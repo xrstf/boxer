21c1396d8d3beecafae1ee9fcede0960c736ed36
xrstf <git@xrstf.de>
2015-10-11 17:41:15 +0200

    use scrypt instead of PBKDF2, use a larger salt (I misread the specs; they say "at least 8 bytes")

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Boxer - NaCl for Humans
 This Go package implements a convenience wrapper around the NaCl package.
 
 Its main goal is to hide the management of nonces and salts from the user.
-Passwords will be automatically stretched using PBKDF2 with SHA-256, the
-nonce includes the current time to make collisions less likely.
+Passwords will be automatically stretched using scrypt, the nonce includes
+the current time to make collisions less likely.
 
 Note that the key stretching is performed on each encryption/decryption, so
 if you plan to perform a lot of crypto operations in a short time, this might
@@ -14,8 +14,8 @@ be too expensive.
 Disclaimer
 ----------
 
-I am by no means a security expert. Do not blindly trust this code, read it
-and judge by yourself. If you find a flaw, please open an issue and tell me.
+I am by no means a cryptography expert. Do not blindly trust this code, read
+it and judge by yourself. If you find a flaw, please open an issue and tell me.
 
 On that note: This package is *very* opinionated. It solves a very narrow
 usecase and should not be applied to every problem that sounds like "I need
--- boxer.go
+++ boxer.go
@@ -3,8 +3,8 @@
 // Package boxer implements a convenience wrapper around the NaCl package.
 //
 // Its main goal is to hide the management of nonces and salts from the user.
-// Passwords will be automatically stretched using PBKDF2 with SHA-256, the
-// nonce includes the current time to make collisions less likely.
+// Passwords will be automatically stretched using scrypt, the nonce includes
+// the current time to make collisions less likely.
 //
 // Note that the key stretching is performed on each encryption/decryption, so
 // if you plan to perform a lot of crypto operations in a short time, this might
@@ -13,18 +13,17 @@ package boxer
 
 import (
 	"crypto/rand"
-	"crypto/sha256"
 	"encoding/binary"
 	"errors"
 	"fmt"
 	"time"
 
 	"golang.org/x/crypto/nacl/secretbox"
-	"golang.org/x/crypto/pbkdf2"
+	"golang.org/x/crypto/scrypt"
 )
 
-// The length of the salt used for key derivation via PBKDF2.
-const SaltLength = 8
+// The length of the salt used for scrypt.
+const SaltLength = 24
 
 // The length of the nonce used for the secretbox implementation.
 const NonceLength = 24
@@ -32,21 +31,48 @@ const NonceLength = 24
 // The length of the encryption key for the secretbox implementation.
 const KeyLength = 32
 
+type ScryptParameters struct {
+	// cost parameter
+	Cost int
+
+	// R value
+	R int
+
+	// P value
+	P int
+}
+
+func DefaultScryptParameters() ScryptParameters {
+	return ScryptParameters{16384, 8, 1}
+}
+
+type Boxer struct {
+	params ScryptParameters
+}
+
+func NewBoxer(params ScryptParameters) *Boxer {
+	return &Boxer{params}
+}
+
+func NewDefaultBoxer() *Boxer {
+	return &Boxer{DefaultScryptParameters()}
+}
+
 // Encrypt some data with a password
 //
 // This function automatically stretches the password to meet the KeyLength
 // requirement, as well as calculate a fresh nonce. The function returns an
 // error if not enough data is avialable in rand.Reader, otherwise the first
 // value will be the encryption result, containing the salt and nonce.
-func Encrypt(data []byte, password []byte) ([]byte, error) {
+func (b *Boxer) Encrypt(data []byte, password []byte) ([]byte, error) {
 	// derive a new encryption key for this message
-	key, salt, err := DeriveKeyFromPassword(password)
+	key, salt, err := b.DeriveKeyFromPassword(password)
 	if err != nil {
 		return nil, errors.New("Could not derive encryption key from password: " + err.Error())
 	}
 
 	// create a fresh nonce
-	nonce, err := CreateNonce()
+	nonce, err := b.CreateNonce()
 	if err != nil {
 		return nil, errors.New("Could not create nonce: " + err.Error())
 	}
@@ -65,10 +91,10 @@ func Encrypt(data []byte, password []byte) ([]byte, error) {
 // Decrypt data
 //
 // This function decrypts data originally encrypted using Encrypt(). It will extract
-// the PBKDF2 salt and nonce and apply the required transformations. An error is
+// the scrypt salt and nonce and apply the required transformations. An error is
 // returned when the ciphertext is too short or the password does not match. Otherwise,
 // the raw decrypted message is returned.
-func Decrypt(ciphertext []byte, password []byte) ([]byte, error) {
+func (b *Boxer) Decrypt(ciphertext []byte, password []byte) ([]byte, error) {
 	minLength := SaltLength + NonceLength + secretbox.Overhead + 1
 
 	if len(ciphertext) < minLength {
@@ -82,8 +108,12 @@ func Decrypt(ciphertext []byte, password []byte) ([]byte, error) {
 	copy(salt[:], ciphertext[:SaltLength])
 	copy(nonce[:], ciphertext[SaltLength:(SaltLength+NonceLength)])
 
+	key, err := b.DeriveKey(password, salt)
+	if err != nil {
+		return nil, err
+	}
+
 	box := ciphertext[(SaltLength + NonceLength):]
-	key := DeriveKey(password, salt)
 
 	plain, success := secretbox.Open(nil, box, nonce, key)
 	if !success {
@@ -95,14 +125,14 @@ func Decrypt(ciphertext []byte, password []byte) ([]byte, error) {
 
 // Derive a key for NaCl's secretbox from a password
 //
-// secretbox requires a key with exactly 32 bytes. This function uses PBKDF2 to
+// secretbox requires a key with exactly 32 bytes. This function uses scrypt to
 // derive a key from a given password. Note that the result is randomized, as it
 // contains a random salt.
 // The first byte slice is the key, the second one is the generated salt. You need
 // to keep the salt (it does not need to be encrypted, consider it "public") and
 // use it again to re-create the same key from a password.
 // If you use Encrypt() and Decrypt(), all this is already taken care of.
-func DeriveKeyFromPassword(password []byte) (*[KeyLength]byte, *[SaltLength]byte, error) {
+func (b *Boxer) DeriveKeyFromPassword(password []byte) (*[KeyLength]byte, *[SaltLength]byte, error) {
 	// create the salt for key derivation
 	salt := new([SaltLength]byte)
 
@@ -111,18 +141,29 @@ func DeriveKeyFromPassword(password []byte) (*[KeyLength]byte, *[SaltLength]byte
 		return nil, nil, errors.New("Could not gather sufficient random data to perform encryption: " + err.Error())
 	}
 
-	return DeriveKey(password, salt), salt, nil
+	key, err := b.DeriveKey(password, salt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return key, salt, nil
 }
 
 // Derive a key from a password and salt
 //
-// This is just a wrapper around PBKDF2 with 8192 iterations and using SHA-256.
-func DeriveKey(password []byte, salt *[SaltLength]byte) *[KeyLength]byte {
-	// create encryption key (32byte) from the password using PBKDF2 (RFC 2898)
+// This is just a wrapper around scrypt.
+func (b *Boxer) DeriveKey(password []byte, salt *[SaltLength]byte) (*[KeyLength]byte, error) {
+	// create encryption key (32byte) from the password
 	key := new([KeyLength]byte)
-	copy(key[:], pbkdf2.Key(password, salt[:], 8192, KeyLength, sha256.New))
 
-	return key
+	result, err := scrypt.Key(password, salt[:], b.params.Cost, b.params.R, b.params.P, KeyLength)
+	if err != nil {
+		return nil, err
+	}
+
+	copy(key[:], result)
+
+	return key, nil
 }
 
 // Create a nonce
@@ -132,7 +173,7 @@ func DeriveKey(password []byte, salt *[SaltLength]byte) *[KeyLength]byte {
 // This implementation returns a nonce consisting of the current time including
 // milliseconds and 16 random bytes. It returns an error if rand.Reader could not
 // provide enough entropy.
-func CreateNonce() (*[NonceLength]byte, error) {
+func (b *Boxer) CreateNonce() (*[NonceLength]byte, error) {
 	nonce := new([NonceLength]byte)
 	now := time.Now().UnixNano()
 
